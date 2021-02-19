2541df4b9b553d645a28359cf52941527a9d5192
sebastien <64170102+dehaut-dev@users.noreply.github.com>
2021-02-19 18:49:44 +0100

    Initial commit

---
+++ index copy.php
@@ -0,0 +1,78 @@
+<?php require './directory/import/header.html'; ?>
+
+<body>
+
+    <header class='container-fluid' id="header-p">
+        <div >
+            <img src="./images/LOGO-RBH-VERTICAL-CANNES-SANS-FOND-BLANC1.png" alt="Rado Beach Helen" id="image__header">
+            <div class="container" id="container__header">
+                <p class="text-light h1 d-flex justify-content-center text-uppercase" id="title__header"> evénementiel</p>
+            </div>
+        </div>
+    </header>
+
+<section id="privatisation" class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-sm-6">
+            <img src="./images/Kairos_3492-RL-1.jpg" alt="" srcset="" class="img__body img-fluid mx-auto d-block">
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <h2>Privatisation du lieu</h2>
+            <p>Vous souhaitez organiser un événement sur une plage de La Croisette ? Rado Beach Helen est le lieu idéal.
+            </p>
+            <p>Dans un cadre accueillant et convivial, nous vous créerons des souvenirs inoubliables en privatisant nos
+                espaces pour un ou plusieurs jours.</p>
+        </div>
+
+    </div>
+</section>
+<section id="evenement" class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Votre événement sur notre plage de La Croisette</h2>
+            <p>Elaboré par les équipes RBH ou Helen, nous concevrons des prestations sur-mesure et mettrons en œuvre
+                toute notre expérience pour préparer et servir ce qui deviendra un moment d’exception</p>
+            <p>Tous vos événements sont réalisables au Rado Beach Helen, toute l’année, selon le temps, en privatisation
+                totale ou partielle : breakfast, déjeuner, dîner, cocktail déjeunatoire ou dînatoire, lancement de
+                produit, événement privé ou corporate…</p>
+        </div>
+        <div class="col">
+            <img src="./images/Kairos_3678-RL.jpg" alt="" srcset="" class="img__body mx-auto d-block">
+        </div>
+    </div>
+</section>
+
+<section class="container" id="possibilite">
+    <div class="row">
+        <div class="col-md-3"><img src="./images/icon/1.png"></img>
+            <p>Possibilité de monter des tentes sur la plage (hors juillet-août).</p>
+        </div>
+        <div class="col-md-3"><img src="./images/icon/2.png"></img>
+            <p>Possibilité de mettre toute la plage aux couleurs et à l’image de votre entreprise*.</p>
+        </div>
+        <div class="col-md-3"><img src="./images/icon/3.png"></img>
+            <p>Parkings bus et voitures à proximité.</p>
+        </div>
+        <div class="col-md-3"><img src="./images/icon/4.png"></img>
+            <p>Possibilité de mettre en place un système de navette pour amener les invités.</p>
+        </div>
+    </div>
+    <div>
+        <p>*En tenant compte de la réglementation en vigueur.</p>
+    </div>
+
+</section>
+
+<section class="container" id="contact">
+    <h3>Contactez-nous</h3>
+    <p><img src="./images/icon/5.png"></img> Olivier Rotondaro<br>
+        orotondaro@radobeachhelen.com<br>
+        +33 (0)6 12 697 220</p>
+    <p>
+        Boulevard de La Croisette<br>
+        06400 Cannes<br>
+        +33 (0)4 93 942 068
+    </p>
+</section>
+<img src="./images/RBH-TENTE-PLAGE.jpg" alt="" class="img-fluid">
+<?php require './directory/import/footer.html'; ?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php require './directory/import/header.html'; ?>
+
+<body>
+
+  <header id="header-p">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./images/nuit.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/TETIERE-RESTAURANT.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/Restaurant-nuit.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+    </div>
+    <img src="./images/LOGO-RBH-VERTICAL-CANNES-SANS-FOND-BLANC1.png" alt="Rado Beach Helen" id="image__header">
+
+    </div>
+  </header>
+
+  <section></section>
+
+  <section></section>
+
+  <section></section>
+
+  <section></section>
+
+  <?php require './directory/import/footer.html'; ?>
