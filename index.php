90cc35278133088d4c3a71955bd5d42fc1738bc8
sebastien <64170102+dehaut-dev@users.noreply.github.com>
2021-02-19 19:18:27 +0100

    maj

--- index.php
+++ index.php
@@ -3,16 +3,26 @@
 <body>
 
   <header id="header-p">
+    </div>
+      <img src="./images/LOGO-RBH-VERTICAL-CANNES-SANS-FOND-BLANC1.png" alt="Rado Beach Helen" id="image__header2">
+    </div>
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./images/nuit.jpg" class="d-block w-100" alt="...">
+          <img src="./images/header/header1.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="./images/TETIERE-RESTAURANT.jpg" class="d-block w-100" alt="...">
+          <img src="./images/header/header2.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="./images/Restaurant-nuit.jpg" class="d-block w-100" alt="...">
+          <img src="./images/header/header3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/header/header4.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/header/header5.jpg" class="d-block w-100" alt="...">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
@@ -23,10 +33,7 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-    </div>
-    <img src="./images/LOGO-RBH-VERTICAL-CANNES-SANS-FOND-BLANC1.png" alt="Rado Beach Helen" id="image__header">
-
-    </div>
+    
   </header>
 
   <section></section>
